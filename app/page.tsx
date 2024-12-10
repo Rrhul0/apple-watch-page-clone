07bb1b349b33d4695bab34ecb0716bd5d923a679
Rahul Raj <rahulraj1417@gmail.com>
2024-12-10 21:37:27 +0530

    fix: added intro animation when click get started

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,63 @@ import Link from 'next/link'
 import Image from 'next/image'
 import LogoImage from '../public/apple-watch-design-studio-logo.jpeg'
 import ChevronDown from './icons/chevronDown'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { Stage } from './constants'
 import GreetingWatchCase from '../public/GreetingWatchCase.png'
 import GreetingWatchBand from '../public/GreetingWatchBand.jpeg'
+import { animate } from 'motion'
 
 export default function Home() {
     const [stage, setStage] = useState<Stage>('intro')
+    const introImageWrapper = useRef<HTMLDivElement>(null)
+    const introGreeting = useRef<HTMLDivElement>(null)
+    const footer = useRef<HTMLDivElement>(null)
+    const pageRef = useRef<HTMLDivElement>(null)
+
+    const onClickStart = () => {
+        if (pageRef.current) pageRef.current.classList.add('hideIntro')
+
+        if (introImageWrapper.current)
+            animate(
+                introImageWrapper.current,
+                {
+                    top: '-19vh',
+                    transform: 'scale(0.5)'
+                },
+                {
+                    duration: 1.5
+                }
+            )
+        if (introGreeting.current)
+            animate(
+                introGreeting.current,
+                {
+                    opacity: 0
+                },
+                {
+                    duration: 0.2
+                }
+            )
+        if (footer.current)
+            animate(
+                footer.current,
+                {
+                    opacity: 1
+                },
+                {
+                    duration: 0.2
+                }
+            )
+
+        setStage('sizes')
+    }
 
     return (
         <div>
-            <div className='page'>
+            <div
+                className='page'
+                ref={pageRef}
+            >
                 <div className='title'>
                     <Link
                         href='/'
@@ -34,7 +80,10 @@ export default function Home() {
                 <div className='mainSection'>
                     <div className='introSection'>
                         <div className='greetingWrapper'>
-                            <div className='greeting'>
+                            <div
+                                className='greeting'
+                                ref={introGreeting}
+                            >
                                 <h1
                                     tabIndex={-1}
                                     className='intro-headline'
@@ -58,13 +107,16 @@ export default function Home() {
                                     tabIndex={0}
                                     type='button'
                                     className='btn-getstarted button'
-                                    onClick={() => setStage('sizes')} //also start the animation
+                                    onClick={onClickStart}
                                 >
                                     Get started
                                 </button>
                             </div>
                         </div>
-                        <div className='imageWrapper'>
+                        <div
+                            className='imageWrapper'
+                            ref={introImageWrapper}
+                        >
                             <div className='combinedimage'>
                                 <Image
                                     src={GreetingWatchCase}
@@ -84,32 +136,29 @@ export default function Home() {
                         </div>
                     </div>
                 </div>
-                {stage !== 'intro' && (
-                    <>
-                        <div className='collections'>
-                            <button type='button'>
-                                <div className='collectionsBlock'>
-                                    Collections
-                                    <span className='visuallyhidden'>
-                                        Apple Watch
-                                    </span>
-                                    <ChevronDown />
-                                </div>
-                            </button>
+                <div className='collectionsContainer'>
+                    <button type='button'>
+                        <div className='collectionsBlock'>
+                            Collections
+                            <span className='visuallyhidden'>Apple Watch</span>
+                            <ChevronDown />
                         </div>
-                        <div className='footer'>
-                            <button
-                                className='button savebtn'
-                                type='button'
-                            >
-                                Save
-                                <span className='visuallyhidden'>
-                                    Apple Watch Style
-                                </span>
-                            </button>
-                        </div>
-                    </>
-                )}
+                    </button>
+                </div>
+                <div
+                    className='save-btn-container'
+                    aria-hidden='true'
+                >
+                    <button
+                        className='button'
+                        type='button'
+                    >
+                        Save
+                        <span className='visuallyhidden'>
+                            Apple Watch Style
+                        </span>
+                    </button>
+                </div>
             </div>
         </div>
     )
